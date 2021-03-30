750580b5b02ed5c7d86118e08faaf8f9daeebfe8
acbk2b <acbecker27@gmail.com>
2021-03-29 19:22:27 -0500

    Added default character

--- external.ts
+++ external.ts
@@ -5,8 +5,18 @@ import { Subtask, RepeatableTask } from './datastructures/tasks.js';
 import { Skill } from './datastructures/skills.js';
 import { ipcRenderer } from 'electron';
 
+var rt = new RepeatableTask("Task Title", "Task Desc", curr_date, 100, [], 10, 10);
+
+var skill: Skill = new Skill("Skill Title", "Skill Desc", 100, [rt]);
+
+var curr_date: Date = new Date();
+var char: Character = new Character("Character1", 500, [rt], [skill]);
+char.save();
+
 var data = getData();
 var character: Character = characterFromObj(data);
+console.log(character);
+console.log(character.skills);
 
 window.onload = () => {
   document.getElementById("charName").innerHTML = character.name;
