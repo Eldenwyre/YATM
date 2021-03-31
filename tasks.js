8d0223070745b9ac920aeed40f8844e40f09a88b
lagm4c <lagm4c@umsystem.edu>
2021-03-31 13:05:28 -0500

    Included the updated tasks.js

---
+++ tasks.js
@@ -0,0 +1,63 @@
+"use strict";
+exports.__esModule = true;
+var json_io_js_1 = require("./json_io.js");
+var data = json_io_js_1.getData();
+var character = json_io_js_1.characterFromObj(data);
+window.onload = function () {
+    for (var i = 0; i < character.tasks.length; i++) {
+        var task = character.tasks[i];
+        var Task = document.createElement('div');
+        var img = document.createElement('img');
+        var Name = document.createElement('h3');
+        var Desc = document.createElement('p');
+        var Datetime = document.createElement('h3');
+        var Subtasks = document.createElement('div');
+        var Reward = document.createElement('h3');
+        var Repeat_increment = document.createElement('div');
+        var Num_repeats = document.createElement('div');
+        const button = document.createElement('button');
+        button.innerText = 'Completed';
+        button.onclick = remove;
+
+        //var Completion = document.createElement("input");
+        Task.className = 'taskTab';
+        // Temporary use of the skill image as the background
+        img.src = "images/Skill.png";
+        Name.className = 'taskName';
+        Name.innerHTML = task.task.title;
+        Desc.className = 'taskDesc';
+        Desc.innerHTML = task.task.description;
+        Datetime.className = 'taskDate';
+        var date = task.task.date.toString();
+        Datetime.innerHTML = date;
+        // Add more to subtasks in the future
+        Subtasks.className = 'taskSub';
+        Reward.className = 'taskReward';
+        Reward.innerHTML = 'Reward';
+        Repeat_increment.className = 'taskInc';
+        Repeat_increment.innerHTML = task.repeat_increment.toString();
+        Num_repeats.className = 'taskRep';
+        Num_repeats.innerHTML = task.num_repeats.toString();
+
+        //Completion.className = 'completeButton';
+        //Completion.onclick = remove;
+        Task.appendChild(img);
+        Task.appendChild(Name);
+        Task.appendChild(Desc);
+        Task.appendChild(Datetime);
+        Task.appendChild(Subtasks);
+        Task.appendChild(Reward);
+        Task.appendChild(Repeat_increment);
+        Task.appendChild(Num_repeats);
+        Task.appendChild(button);
+        //Task.appendChild(Completion);
+        document.getElementsByTagName('body')[0].appendChild(Task);
+
+        function remove(event){
+          alert("Remove this task");
+        //  document.getElementsByTagName('body')[0]removeChild(document.getElementsByTagName('body')[0][i]);
+        //  const ipcRenderer = require('electron').ipcRenderer;
+        //  ipcRenderer.send("taskrefresh");
+        }
+    }
+};
